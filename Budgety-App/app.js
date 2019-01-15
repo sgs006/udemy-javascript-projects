5c081ad0a99a4999a89991c2deb6aeccc40a081e
Sam Stokes <sgstokes006@gmail.com>
2019-01-14 19:43:20 -0600

    updating folders

---
+++ Budgety-App/app.js
@@ -0,0 +1,26 @@
+//BUDGET CONTROLLER
+var budgetController = (function() {})();
+
+//UI CONTROLLER
+var UIController = (function() {
+  // Some code
+})();
+
+//GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl) {
+  var ctrlAddItem = function() {
+    //1. Get the filed input data
+    //2. Add the item to the budget controller
+    //3. Add the item to the UI
+    //4. Calculate the budget
+    //5. Display the budget on the UI
+    console.log("get er done");
+  };
+
+  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+  document.addEventListener("keypress", function(e) {
+    if (e.keyCode === 13 || e.which === 13) {
+      ctrlAddItem();
+    }
+  });
+})(budgetController, UIController);
