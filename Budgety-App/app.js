4cc0bb99a927744c563c0851b998b3e5ab6f48b1
Samuel Stokes <sgstokes006@gmail.com>
2019-01-18 11:17:16 -0600

    Clear Fields Function
    Created function in UIController to clear fields after input is entered. It also resets the cursor to the description box to make multiple inputs easier. Invoke the clearFields function in the controller as part of the ctrlAddItem function.

--- Budgety-App/app.js
+++ Budgety-App/app.js
@@ -94,6 +94,20 @@ var UIController = (function() {
       //Insert HTLM into the DOM
       document.querySelector(element).insertAdjacentHTML("beforeend", newHTML);
     },
+     //clears input fields for next input
+    clearFields: function(){
+      var fields, fieldsArray;
+      //creates list of values
+      fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+      //converts list of values to array so we can use forEach
+      fieldsArray = Array.prototype.slice.call(fields);
+      //Loops over our array setting all values to empty
+      fieldsArray.forEach(function (current, index, array) {
+        current.value = '';
+      })
+      //set cursor back to description box
+      fieldsArray[0].focus();
+    },
     //make DOMstrings object accessible outside UIController scope.
     getDOMstrings: function() {
       return DOMstrings;
@@ -129,9 +143,11 @@ var controller = (function(budgetCtrl, UICtrl) {
     newItem = budgetCtrl.addItem(input.type, input.description, input.value);
     //3. Add the item to the UI
     UICtrl.addListItem(newItem, input.type);
+    //4. Clear the fields
+    UICtrl.clearFields();
 
-    //4. Calculate the budget
-    //5. Display the budget on the UI
+    //5. Calculate the budget
+    //6. Display the budget on the UI
   };
 
   return {
