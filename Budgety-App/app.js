c28be0e0edbf7d0a42efe395be8a4f42b19f54a7
Sam Stokes <sgstokes006@gmail.com>
2019-02-12 20:31:09 -0600

    Update UI Percentages for Expenses
    Wrote functions to loop over nodelist in order to append percentages to correct positions in array and display to UI

--- Budgety-App/app.js
+++ Budgety-App/app.js
@@ -145,7 +145,8 @@ var UIController = (function () {
     incomeLabel: ".budget__income--value",
     expensesLabel: ".budget__expenses--value",
     percentageLabel: ".budget__expenses--percentage",
-    container: ".container"
+    container: ".container",
+    expensesPercLabel: ".item__percentage"
   };
 
   return {
@@ -217,6 +218,26 @@ var UIController = (function () {
       }
     },
 
+    displayPercentages: function (percentages) {
+      //returns NodeList
+      var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+
+      var nodeListForEach = function (list, callback) {
+        for (var i = 0; i < list.length; i++) {
+          callback(list[i], i);
+        }
+      };
+
+      nodeListForEach(fields, function (current, index) {
+        if (percentages[index] > 0) {
+          current.textContent = percentages[index] + '%';
+        } else {
+          current.textContent = '---';
+        }
+      });
+
+    },
+
     //make DOMstrings object accessible outside UIController scope.
     getDOMstrings: function () {
       return DOMstrings;
@@ -260,7 +281,7 @@ var controller = (function (budgetCtrl, UICtrl) {
     //2. Read percentages from the budget controller
     var percentages = budgetCtrl.getPercentages();
     //3. Update the UI with the new percentages
-    console.log(percentages);
+    UICtrl.displayPercentages(percentages);
 
   };
 
